95fbe03e2d7c21b00ffe42168078dee0fa0f0471
emanuele-ghica <emanuele.ghica@gmail.com>
2024-10-20 17:46:55 +0300

    Created a API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]   // /api/users   ==> [controller] is replaced with the part before "Controller" when looking at a class
+public class UsersController(DataContext context) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {       // a method to return all the users
+        var users = await context.Users.ToListAsync();                       // IEnumerable emans that the data received(AppUser) is represented by a collection of elements
+                                                                             // that can be enumerated(looped through)
+        return users;
+    }
+
+    [HttpGet("{id:int}")]  // /api/users/3    we need curly braces {}  because then it's gonna be the values of the id instead of the "id"
+      public async Task<ActionResult<AppUser>> GetUsers(int id) {   // using Action Result allows us to return different response types on the same action method
+        var users = await context.Users.FindAsync(id);              // Users.find(id) can return null.
+
+        if(users == null) return NotFound();                        // we check here if the user returned is null and if it is we send a 404 Not Found to the client
+
+        return users;                                               // now the compiler knows that users cannot be null here
+    }
+}

2a8de6a3e8e79484fa801ad1398f240c047a9eb0
emanuele-ghica <emanuele.ghica@gmail.com>
2024-10-29 17:28:04 +0200

    Added Exception middleware on API and error toastr on client side

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,7 @@ using System.Xml.Linq;
 using API.Data;
 using API.Extensions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,7 @@ builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-test-errors',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-errors.component.html',
+  styleUrl: './test-errors.component.css'
+})
+export class TestErrorsComponent implements OnDestroy{
+ 
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+  private subcription = new Subscription();
+  validationErrors: string[] = [];
+
+  get400Error() {
+   const error400Subscription = this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+    next: response => console.log(response),
+    error: error => console.log(error), 
+   });
+   this.subcription.add(error400Subscription);
+  };
+
+  get401Error() {
+    const error401Subscription = this.http.get(this.baseUrl + 'buggy/auth').subscribe({
+     next: response => console.log(response),
+     error: error => console.log(error), 
+    });
+    this.subcription.add(error401Subscription);
+   };
+
+   get404Error() {
+    const error404Subscription = this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+     next: response => console.log(response),
+     error: error => console.log(error), 
+    });
+    this.subcription.add(error404Subscription);
+   };
+
+   get500Error() {
+    const error500Subscription = this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
+     next: response => console.log(response),
+     error: error => console.log(error), 
+    });
+    this.subcription.add(error500Subscription);
+   };
+
+   get400ValidationError() {
+    const error400ValidationSubscription = this.http.post(this.baseUrl + 'account/register', {}).subscribe({
+     next: response => console.log(response),
+     error: error => {
+      console.log(error);
+      this.validationErrors = error;
+    }, 
+    });
+    this.subcription.add(error400ValidationSubscription);
+   };
+
+  ngOnDestroy(): void {
+   this.subcription.unsubscribe();
+  };
+}

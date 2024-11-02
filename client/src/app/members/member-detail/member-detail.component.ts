f6a384738127ca2864c6df96865365d0527afc3d
emanuele-ghica <emanuele.ghica@gmail.com>
2024-11-02 13:21:59 +0200

    Added a gallery after updating the angular version

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -4,11 +4,12 @@ import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { Subscription } from 'rxjs';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -18,6 +19,7 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
   private route = inject(ActivatedRoute);
   member?: Member;
   private subsciption = new Subscription();
+  images: GalleryItem[] = [];
 
   ngOnInit() : void {
     this.loadMember();
@@ -27,7 +29,12 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
     const username = this.route.snapshot.paramMap.get('username');
     if(!username) return;
     const loadMemberSubscription = this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member
+        member.photos.map(p => {
+          this.images.push(new ImageItem({src: p.url, thumb: p.url}))
+        })
+      }
     })
     this.subsciption.add(loadMemberSubscription);
   } 
